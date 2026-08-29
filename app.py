f9d4a0c57e6e7709a0e117123ee2307a2bbd8bbb
nawaz <nawaz.shaik3@gmail.com>
2026-08-29 09:48:00 +0530

    added vote reset script

--- app.py
+++ app.py
@@ -31,6 +31,14 @@ def vote(name):
 def results():
     return jsonify(votes)
 
+@app.route("/reset")
+def reset():
+    votes.clear()
+
+    return jsonify({
+        "message": "All votes have been reset"
+    })
+
 if __name__ == "__main__":
     app.run(host="localhost", port=5000, debug=True)
     
